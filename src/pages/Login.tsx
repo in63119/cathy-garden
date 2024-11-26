973b4ec9d0a7b29dad0c0c8d9f689e5ba95b96b9
이인수 <in63119@gmail.com>
2024-11-27 00:05:50 +0900

    chore: baseUrl & paths 적용

---
+++ src/pages/Login.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+// MUI css
+import { Box, LinearProgress } from "@mui/material";
+
+export default function Login() {
+  return (
+    <Box>
+      <LinearProgress color="inherit" />
+    </Box>
+  );
+}

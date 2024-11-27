8a51e7d3dbe2a0f0c90e6947d5b792b4f87f61a1
이인수 <in63119@gmail.com>
2024-11-27 11:43:41 +0900

    fix: ico & icon images

--- contract/contracts/Login.sol
+++ contract/contracts/Login.sol
@@ -1,11 +1,13 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.27;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Login {
+
+contract Login is Ownable{
     uint a = 0;
 
-    constructor() {}
+    constructor() Ownable(msg.sender) {}
 
     function get() view public returns(uint) {
         return a;

64d4f41c38856e3ad148ef056f9df0ef0abf6f74
이인수 <in63119@gmail.com>
2024-11-27 10:50:41 +0900

    contract init setting

---
+++ contract/contracts/Login.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.27;
+
+
+contract Login {
+    uint a = 0;
+
+    constructor() {}
+
+    function get() view public returns(uint) {
+        return a;
+    }
+}

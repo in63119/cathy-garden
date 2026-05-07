48a46b31441900f88f4c1271a809430de5582d44
inbrew <inbrew@inbrewui-MacBookAir.local>
2026-05-07 20:52:50 +0900

    chore: stop tracking next build artifacts

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Z762U6Ju9gon0bwn9FnJUxbA5LDOoX/wiyCDcv++BSk=\"\n}"
--- .next/server/chunks/ssr/_next-internal_server_app_media_[id]_page_actions_03wqv4z.js
+++
@@ -1,3 +0,0 @@
-module.exports=[50829,(a,b,c)=>{}];
-
-//# sourceMappingURL=_next-internal_server_app_media_%5Bid%5D_page_actions_03wqv4z.js.map
